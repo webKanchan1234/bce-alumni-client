360cf6684606963648f8b4717d91a6b29b75ebb5
webKanchan1234 <kanchankr15153795@gmail.com>
2026-06-20 12:05:29 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://bce-alumni-server.onrender.com/api",
 });
 
 api.interceptors.request.use(
